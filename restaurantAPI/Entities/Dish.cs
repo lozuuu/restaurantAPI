f4aca413a05a4c4117b614de9d096c43bfb0ce91
kacpe <kacpe@LOZINSKI>
2024-07-24 07:53:44 +0200

    Dodaj pliki projektów.

---
+++ restaurantAPI/Entities/Dish.cs
@@ -0,0 +1,15 @@
+﻿namespace restaurantAPI.Entities
+{
+    public class Dish
+    {
+        int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int RestaurantId { get; set; }
+
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
---
+++ restaurantAPI/Entities/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿namespace restaurantAPI.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public bool HasDelivery { get; set; }
+
+        public string ContatEmail { get; set; }
+
+        public string ContactNumber {  get; set; } 
+
+        public int AddressId { get; set; }
+
+         public virtual Addres Addres { get; set; }
+
+        public virtual List<Dish> Dishes { get; set; }
+
+
+    }
+}
