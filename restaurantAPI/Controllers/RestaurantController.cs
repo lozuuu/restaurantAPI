0b9f4dd89e902d44154643e73bb7b5ec2b9c4186
Kacper <173662@o365.prz.edu.pl>
2024-07-29 18:18:52 +0200

    dokończyć modele

--- restaurantAPI/Controllers/RestaurantController.cs
+++ restaurantAPI/Controllers/RestaurantController.cs
@@ -1,29 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using restaurantAPI.Entities;
+using restaurantAPI.Models;
 
 namespace restaurantAPI.Controllers
 
 {
-    [Route("api/restaurant")]
+    [Route("restaurant")]
 
     public class RestaurantController: ControllerBase
     {
         private readonly RestaurantDbContext _dbContext;
 
-        public RestaurantController(RestaurantDbContext dbContext) {
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
             _dbContext = dbContext;       
         }
 
-        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
             var restaurants = _dbContext
                 .Restaurants
                 .ToList();
+            var restaurantsDtos = restaurants.Select(r => new RestaurantDto()
+            {
+                
 
-            return Ok(restaurants);
+            }
+            return Ok(restaurantsDtos);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .FirstOrDefault(r => r.Id == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
 
-
-
+            else
+            {
+                return Ok(restaurant);
+            }
         }
     }
+   
 }
+
+    
